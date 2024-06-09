be41df0b18c6cdeb2484303fc44ef666b9711d6c
Alvee <M.Alvee8141@gmail.com>
2024-06-09 21:22:53 +0600

    updates

--- www.suprasy.com/src/components/layout/Layout.tsx
+++ www.suprasy.com/src/components/layout/Layout.tsx
@@ -8,8 +8,8 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
       <div className="relative mx-auto lg:container lg:px-16 xl:px-20 h-fit">
         <Header />
         <main>{children}</main>
+        <Footer />
       </div>
-      <Footer />
     </main>
   );
 };
