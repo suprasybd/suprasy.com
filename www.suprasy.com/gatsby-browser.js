96f49a5c9193426fbf71a8118b77ee9fbab9b0f6
Alvee <m.alvee8141@gmail.com>
2024-06-27 20:31:34 +0600

    Update gatsby-browser.js

--- www.suprasy.com/gatsby-browser.js
+++ www.suprasy.com/gatsby-browser.js
@@ -1,1 +1,11 @@
 import './src/styles/globals.css';
+
+export const onClientEntry = () => {
+  const script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.async = true;
+  script.src = 'https://embed.tawk.to/667d7627eaf3bd8d4d150266/1i1d1b2m5';
+  script.charset = 'UTF-8';
+  script.setAttribute('crossorigin', '*');
+  document.body.appendChild(script);
+};
